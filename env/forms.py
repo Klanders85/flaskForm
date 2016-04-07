7443e6af139baf040c8807cf002b4c7cee793855
kellylanders2@gmail.com <kellylanders@Kellys-MacBook-Pro-2.local>
2016-04-06 22:07:33 -0500

    add about view copy

--- env/forms.py
+++ env/forms.py
@@ -5,10 +5,10 @@
 # from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
 
 # # # create a Class named ContactForm which will inherit the Form Class
-# class ContactForm(Form):
-# 	# properties of the Class
-# 	name  = TextField("Name", [validators.Required()])
-# 	email = TextField("Email", [validators.Required(), validators.Email()])
-# 	subject = TextField("Subject", [validators.Required()])
-# 	message = TextAreaField("Message", [validators.Required()])
-# 	submit = SubmitField("Send")
+class ContactForm(Form):
+	# properties of the Class
+	name  = TextField("Name", [validators.Required()])
+	email = TextField("Email", [validators.Required(), validators.Email()])
+	subject = TextField("Subject", [validators.Required()])
+	message = TextAreaField("Message", [validators.Required()])
+	submit = SubmitField("Send")
